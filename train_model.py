9bd908d903a8c1d09e94901b2f96f49a7e0a1dba
abdessamadelhajjami <149787830+abdessamadelhajjami@users.noreply.github.com>
2024-07-01 11:19:22 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -9,14 +9,19 @@ def get_updates():
     if response.status_code == 200:
         updates = response.json()
         print(updates)
+        return updates
     else:
         print(f"Échec de l'obtention des mises à jour: {response.text}")
+        return None
 
 # Tester l'obtention de mises à jour
 if __name__ == "__main__":
-    get_updates()
-
-
+    updates = get_updates()
+    if updates and "result" in updates and len(updates["result"]) > 0:
+        for update in updates["result"]:
+            print(update)
+    else:
+        print("Aucune mise à jour trouvée.")
 
 
 
