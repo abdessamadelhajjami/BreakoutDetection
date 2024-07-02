fd0eccc72df90d248d7c0711d7b15720c50cba71
abdessamadelhajjami <149787830+abdessamadelhajjami@users.noreply.github.com>
2024-07-02 14:27:15 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -314,7 +314,7 @@ if __name__ == '__main__':
         today_idx = df.index[-1]
         breakout_type, slope, intercept = isBreakOut(df, today_idx)
         print(f"breakout type today for {symbol} is: {breakout_type}")
-        
+        breakout_type = 1
         if breakout_type > 0:
             print("YEPP1")
             features = extract_and_flatten_features(df, today_idx)
