e77a8b0f5bdd6c3dfc0f278464f0c82ad89087ab
abdessamadelhajjami <149787830+abdessamadelhajjami@users.noreply.github.com>
2024-07-05 20:04:24 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -307,12 +307,16 @@ def detect_and_label_breakouts(df):
 
 def train_and_save_model(df, table_name):
 
+   
     model_filename = f"{table_name}_model.pkl"
     scaler_filename = f"{table_name}_scaler.pkl"
+    imputer_filename = f"{table_name}_imputer.pkl"
 
-    if os.path.exists(model_filename) :
-        print(f"Model and scaler for {table_name} already exist. Skipping training.")
+    if os.path.exists(model_filename) and os.path.exists(scaler_filename) and os.path.exists(imputer_filename):
+        print(f"Model, scaler, and imputer for {table_name} already exist. Skipping training.")
         return
+
+        
     print(f"Data from {table_name}:")
     print(df.head())
 
