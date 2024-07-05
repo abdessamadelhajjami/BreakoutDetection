276008d98c135656d2bb2abab76b89f8f6c45ee7
abdessamadelhajjami <149787830+abdessamadelhajjami@users.noreply.github.com>
2024-07-05 15:41:58 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -409,7 +409,8 @@ def main():
         schema=SNOWFLAKE_CONN['schema']
     )
 
-    symbol = 'AAPL'
+    symbol = 'AMZN'
+    
     table_name = f'ohlcv_data_{symbol}'.upper()
     last_date = get_last_date(conn, table_name)
     data = download_sp500_data(symbol, last_date, pd.Timestamp.now().strftime('%Y-%m-%d'))
