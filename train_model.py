9b24f21f5eaaa7d0c0974df4c59f16f36dc3c2d0
abdessamadelhajjami <149787830+abdessamadelhajjami@users.noreply.github.com>
2024-07-05 10:46:46 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -328,7 +328,7 @@ def main():
     )
     print('[MAIN] : Connected to Snowflake for SP500 data.')
 
-    symbol = 'AAPL'
+    symbol = 'MMM'
     table_name = f'ohlcv_data_{symbol}'.upper()
     last_date = get_last_date(conn, SP500_CONN['schema'], table_name)
     data = download_sp500_data(symbol, last_date, pd.Timestamp.now().strftime('%Y-%m-%d'))
